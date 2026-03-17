e073018e3f0f3262ccf102a5db0357adc46dac6c
Ma1the3us <matheusrodriguesilva3@gamail.com>
2026-03-17 12:39:38 -0300

    Adicionando Mentoria como atributo de Curse

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import br.com.dio.desafio.dominio.Curse;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args){
-        Curse Java2023 = new Curse("Java2023","Top",87);
-        Curse JavaScript2023 = new Curse("JavaScript2023","Menus Top",87);
+
+        Mentoria mentor1 =new Mentoria("Matheus","mamamamam@gmail.com");
+        Mentoria mentor2 =new Mentoria("Miguel","mamamamam@gmail.com");
+
+
+        Curse Java2023 = new Curse("Java2023","Top",87,mentor1);
+        Curse JavaScript2023 = new Curse("JavaScript2023","Menus Top",87,mentor2);
         System.out.println(Java2023);
         System.out.println(JavaScript2023);
     }
--- src/br/com/dio/desafio/dominio/Curse.java
+++ src/br/com/dio/desafio/dominio/Curse.java
@@ -4,14 +4,15 @@ public class Curse {
 private String titulo;
     private String descricao;
     private  int cargaHoratia ;
+    private Mentoria mentoria;
 
 
-    public Curse(String titulo, String descricao, int cargaHoratia) {
+    public Curse(String titulo, String descricao, int cargaHoratia,Mentoria  mentoria) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.cargaHoratia = cargaHoratia;
+        this.mentoria= mentoria;
     }
-
 
 
     @Override
@@ -20,6 +21,7 @@ private String titulo;
                 "titulo='" + titulo + '\'' +
                 ", descricao='" + descricao + '\'' +
                 ", cargaHoratia=" + cargaHoratia +
+                ", mentoria=" + mentoria +
                 '}';
     }
 }
